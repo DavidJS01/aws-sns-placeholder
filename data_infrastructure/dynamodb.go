4848e7f169ca49037e03ccf547df2752e4db272c
DavidJS01 <davidshipmanchs2020@gmail.com>
2021-10-11 17:27:01 -0600

    feature(AWS): dynamo db stub

--- data_infrastructure/dynamodb.go
+++ data_infrastructure/dynamodb.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"context"
+	"log"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"fmt"
+)
+
+func main() {
+    cfg, err := config.LoadDefaultConfig(context.TODO())
+if err != nil {
+  log.Fatalf("failed to load configuration, %v", err)
+} else {
+	fmt.Printf("Successfully configured credentials: %v", cfg)
+}
+
+// client := s3.NewFromConfig(cfg)
+
+}
